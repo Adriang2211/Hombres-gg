430d749d0b9fd0f99955d2475f8acdb18d02e29f
Adriang2211 <61050000+Adriang2211@users.noreply.github.com>
2022-04-13 01:31:29 +0200

    Corrección conflictos
    Corrección de conflictos en el anterior commit. Hay algún conflicto en el archivo Piezas.h. Se conserva la versión actualizada y se descarta la anterior.

--- Piezas.h
+++ Piezas.h
@@ -9,6 +9,7 @@
 #include "Posicionamiento.h" //Incluye la clase para el uso del sistema de coordenadas
 #include "Interacciones.h" //Para declarla como amiga de todas las piezas
 
+<<<<<<< Updated upstream
 
 class Torre {
 	friend class Interacciones;
@@ -21,6 +22,11 @@ public:
 	bool getColor() { return color; }
 	void setCoordenadas(int x, int y){ coordenadas.setXY(x, y); }
 	void setCoordenadas(Coords a) { coordenadas = a; }
+=======
+class Torre {
+	Coords coordenadas;
+	bool color; //True=blancas, flase = negras
+>>>>>>> Stashed changes
 };
 
 class Caballo {
