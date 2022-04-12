30a9a3814dd1e40a1b3fd03ebf55f3a6aa4f8727
Adriang2211 <61050000+Adriang2211@users.noreply.github.com>
2022-04-12 20:45:34 +0200

    Cambio del .cpp al .h
    Cambiar las declaraciones de las clases del archivo piezas.cpp al archivo de piezas.h, que por error se habían ubicado en el archivo incorrecto en uno de los últimos commits.

--- Piezas.h
+++ Piezas.h
@@ -1,1 +1,33 @@
 #pragma once
+
+/*
+* En este archivo se crean las clases de cada una de las piezas.
+* Previsiblemente se creará una clase pieza que haciendo uso de hererncia
+* agrupará los aspectos comunes a todas las piezas.
+*/
+
+#include "Posicionamiento.h" //Incluye la clase para el uso del sistema de coordenadas
+
+class Torre {
+
+};
+
+class Caballo {
+
+};
+
+class Alfil {
+
+};
+
+class Dama {
+
+};
+
+class Rey {
+
+};
+
+class Peon {
+
+};
