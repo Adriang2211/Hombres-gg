bd925d03e08f8471282e5658f017fd52c485b857
Adriang2211 <61050000+Adriang2211@users.noreply.github.com>
2022-04-14 13:38:47 +0200

    Sobrecarga << para mostrar coordenadas
    Implementada la sobrecarga del operador << para poder mostrar coordenadas fácilmente.
    Corregidos algunos errores con const en las funciones get de la clase coords

--- src/Posicionamiento.cpp
+++ src/Posicionamiento.cpp
@@ -1,5 +1,5 @@
 #include "Posicionamiento.h"
-
+#include <iostream> //Para sobrecarga de operadores
 
 Coords::Coords(int xi, int yi) {
 	x = xi;
@@ -18,14 +18,59 @@ void Coords::setXY(int xi, int yi) {
 	y = yi;
 }
 
-int const Coords::getX() {
+int Coords::getX() const{
 	return x;
 }
 
-int const Coords::getY() {
+int Coords::getY() const{
 	return y;
 }
 
-Coords const Coords::getXY() {
+Coords Coords::getXY() const{
 	return {x,y};
+}
+
+using namespace std;
+ostream& operator << (ostream& os, const Coords& coordenadas) {
+	os << "[";
+	switch (coordenadas.getX()) {
+	case 0: //Para cuando una pieza se ha comido y se ha "sacado" del tablero
+		os << "#";
+		break;
+	case A:
+		os << "a";
+		break;
+	case B:
+		os << "b";
+		break;
+	case C:
+		os << "c";
+		break;
+	case D:
+		os << "d";
+		break;
+	case E:
+		os << "e";
+		break;
+	case F:
+		os << "f";
+		break;
+	case G:
+		os << "g";
+		break;
+	case H:
+		os << "h";
+		break;
+	default:
+		os << "?";
+
+	}
+	if (coordenadas.getY() > 0 && coordenadas.getY() < 9)
+		os << coordenadas.getY();
+	else if (coordenadas.getY() == 0)
+		os << "#";
+	else
+		os << "?";
+	os << "]";
+	return os;
 }
--- src/Posicionamiento.h
+++ src/Posicionamiento.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 enum enum_x { A = 1, B, C, D, E, F, G, H }; //Equivalencia en letras para utilizar notación de ajedrez
 
@@ -16,8 +17,9 @@ public:
 	void setX(int xi);
 	void setY(int yi);
 	void setXY(int xi, int yi);
-	int const getX();
-	int const getY();
-	Coords const getXY();
+	int getX() const;
+	int getY() const;
+	Coords getXY() const;
+	friend std::ostream& operator << (std::ostream& os, const Coords& coordenada);
 
 };
