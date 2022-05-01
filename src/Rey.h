963c708d0db73ca8970362f47fe3e7ec51884c3f
Adriang2211 <61050000+Adriang2211@users.noreply.github.com>
2022-05-01 12:16:47 +0200

    Carpeta para imágenes y variables primer movimiento
    Carpeta para poder incluir imágenes en la wiki (se incluye un diagrama UML del estado actual del proyecto).
    Variables de tipo bool para saber si el rey y la torre se han movido hasta el momento, para poder programar el enroque posteriormente una vez se hayan definido algunas funciones adicionales que van a ser necesarias para ello.

--- src/Rey.h
+++ src/Rey.h
@@ -6,4 +6,5 @@ class Rey :public Pieza {
 public:
 	//El rey no tiene valor
 	void movimientos(); //Sin implementar
+	bool primer_movimiento; //Para el enroque, el rey no se puede haber movido.
 };
--- src/Torre.h
+++ src/Torre.h
@@ -5,5 +5,6 @@ class Torre :public Pieza {
 public:
 	void movimientos();
 	static const int valor = 5;
+	bool primer_movimiento; //Para el enroque, la torre no se puede haber movido.
 };
 
