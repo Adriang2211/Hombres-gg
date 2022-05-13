ef87ad2b4b3d6920fdcc81a76c71df0ed3374d2d
idopazo <60939695+idopazo@users.noreply.github.com>
2022-05-13 10:18:28 +0200

    prototipo de funcion

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,23 @@
 #include "ETSIDI.h"
 #include "Tablero.h"
 #include "Piezas.h"
+#include <stdio.h>
+using namespace std;
+
 
 void OnDraw(void); //esta funcion sera llamada para dibujar
 void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
-void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla	
+void MouseButton(int button, int down, int x, int y);
 
+void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla	
+int xcell_sel, ycell_sel;			//cell selected with mouse
+
+	//mouse+special-keys combination state events 
+bool controlKey;
+bool shiftKey;
+bool leftButton;
+bool rightButton;
+bool midButton;
 Tablero tablero1; //Pruebas - posición inicial de la partida
 Tablero tablero2; //Pruebas - posición 2
 
@@ -16,7 +28,7 @@ int main(int argc, char* argv[])
 	//Inicializar el gestor de ventanas GLUT
 	//y crear la ventana
 	glutInit(&argc, argv);
-	glutInitWindowSize(900, 900);
+	glutInitWindowSize(800, 600);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutCreateWindow("MiJuego");
 	
@@ -32,7 +44,7 @@ int main(int argc, char* argv[])
 	glutDisplayFunc(OnDraw);
 	glutTimerFunc(25, OnTimer, 0);//le decimos que dentro de 25ms llame 1 vez a la funcion OnTimer(). Por ejemplo una esfera que queremos que se mueva. Cada 25 ms se movera en la direccion que le hemos indicado
 	glutKeyboardFunc(OnKeyboardDown);//Eventos de teclado. Cuando pulsamos una tecla pues se "activa" esta funcion.
-
+	glutMouseFunc(MouseButton);
 	//Inicializar los objetos
 
 
@@ -41,14 +53,14 @@ int main(int argc, char* argv[])
 	//tablero1.actualizarCasillasOcupadas();
 	//tablero1.actualizarMovimientosPosibles();
 	//std::cout << tablero1;
-	
+	/*
 	std::cout << "Prueba movimientos posibles con una partida empezada:" << std::endl << "TABLERO 2:";
 	std::cout << std::endl << "_________________" << std::endl;
 	tablero2.generarTest();
 	tablero2.actualizarCasillasOcupadas();
 	tablero2.actualizarMovimientosPosibles();
 	std::cout << tablero2;
-
+	*/
 
 	//pasarle el control a GLUT,que llamara a los callbacks
 	glutMainLoop();
@@ -64,8 +76,8 @@ void OnDraw(void)
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	gluLookAt(0, 00, 30,  // posicion del ojo
-		0.0, 0, 0.0,      // hacia que punto mira  (0,0,0) 
+	gluLookAt(4, 4, 12,  // posicion del ojo
+		4.0,4, 0.0,      // hacia que punto mira  (0,0,0) 
 		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
 
 	//aqui es donde hay que poner el código de dibujo
@@ -80,11 +92,62 @@ void OnDraw(void)
 }
 void OnKeyboardDown(unsigned char key, int x_t, int y_t)
 {
-	
+	if (key == 'a') cout << "hola";
 
 	//ultima linea, siempre que hay que ponerla:
 	glutPostRedisplay();
 }
+
+
+void sacarcelda(int x, int y, int& cell_x, int& cell_y) {
+	//world coordinates to cell
+	cell_x = (int)(((x-124) / 70)+1);
+	cell_y = (int)(((600-y) / 70)+1);
+}
+
+void MouseButton(int button, int down, int x, int y) {
+	/////////////////////////
+	// sets state of mouse control buttons + special keys
+
+	/////////
+	//computes cell coordinates from mouse coordinates
+	cout << "hola";
+	GLint viewport[4];
+	GLdouble modelview[16];
+	GLdouble projection[16];
+	GLfloat winX, winY, winZ;
+	GLdouble posX, posY, posZ;
+
+	glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+	glGetDoublev(GL_PROJECTION_MATRIX, projection);
+	glGetIntegerv(GL_VIEWPORT, viewport);
+
+	winX = (float)x;
+	winY = (float)viewport[3] - (float)y;
+	glReadPixels(x, int(winY), 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winZ);
+	gluUnProject(winX, winY, winZ, modelview, projection, viewport, &posX, &posY, &posZ);
+
+	//finally cell coordinates
+	sacarcelda(x, y, xcell_sel, ycell_sel);
+
+	///////////////////////////	
+	
+
+	if (button == GLUT_LEFT_BUTTON)
+		leftButton = down;
+	else if (button == GLUT_RIGHT_BUTTON)
+		rightButton = down;
+
+	///////////////////////////
+
+		//***WRITE ACTIONS CONNECTED TO MOUSE STATE HERE
+
+		//print cell coordinates after click
+	if (down)
+		cout << "(" << xcell_sel << "," << ycell_sel << ")" << "("<<x<<" "<<y<<")" << endl;
+
+}
+
 
 void OnTimer(int value)
 {
